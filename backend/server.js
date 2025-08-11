f89a41e90f0c1916c25e4fe24b503f743ffe845f
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-11 23:58:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB error:", err));
