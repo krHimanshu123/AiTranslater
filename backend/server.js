a05caa164bf9897e338073b8e8bce62e23950a33
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-11-02 23:39:21 +0530

    Add log message for server startup

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/translate', translateRoutes);
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
